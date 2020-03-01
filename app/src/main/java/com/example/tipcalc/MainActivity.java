7c09e61bda6714016fec451404f3d8afed861f62
Tbetcha <troy.boettger@gmail.com>
2020-03-01 01:33:24 -0500

    updated checkbox functionality

--- app/src/main/java/com/example/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -8,6 +8,7 @@ import android.text.Editable;
 import android.view.KeyEvent;
 import android.view.KeyEvent.Callback;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,15 +25,15 @@ public class MainActivity extends AppCompatActivity {
     private Button mSubmitTip;
     private TextView mTotal;
     private Button mTotalEnter;
-    public boolean hasTotal;
-
-
-
+    private CheckBox personable;
+    private CheckBox fastService;
+    public boolean hasTotal = false;
+    public boolean personableChecked = false;
+    public boolean fastServiceChecked = false;
 
     private Bill billOne = new Bill();
 
     //TODO change to total -- double totalBill = Double.parseDouble(mGetTip.getText().toString());
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +47,9 @@ public class MainActivity extends AppCompatActivity {
         mSubmitTip = (Button) findViewById(R.id.submit_tip);
         mDisplayTotal = (TextView) findViewById(R.id.display_total);
         mTotalEnter = (Button) findViewById(R.id.total_enter);
+        personable = (CheckBox) findViewById(R.id.personable);
+        fastService = (CheckBox) findViewById(R.id.fast_service);
         //TODO boolean hasTotal = false;
-
-
-
 
         mSubmitTip.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,29 +61,65 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     double tipPer = Double.parseDouble(mGetTip.getText().toString());
                     billOne.setTipAmt(tipPer);
-
                 }
             }
         });
 
+        //TODO disable functionality of checkboxes/total when we have total and bring it back when gone
 
-      mTotalEnter.setOnClickListener(new View.OnClickListener() {
-        @Override
+        mTotalEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 {
-
                     double tot = Double.parseDouble(mEnterTotal.getText().toString());
-                   mDisplayTotal.setText(valueOf(billOne.getTotal( billOne.getTipAmt(),tot)));
-
-
+                    mDisplayTotal.setText(valueOf(billOne.getTotal(billOne.getTipAmt(), tot)));
+                    hasTotal = true;
+                    if(hasTotal == true){
+                        mTotalEnter.setClickable(false);
+                        mSubmitTip.setClickable(false);
+                    }else if(hasTotal == false){
+                        personable.setClickable(false);
+                        fastService.setClickable(false);
+                    }
 
                 }
-
             }
 
         });
-    }
 
+        personable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(billOne.getTipAmt() == 0){
+                    return;
+                }
+                if(personable.isChecked()){
+                    double total = (Double.parseDouble(mDisplayTotal.getText().toString()));
+                   double newWithExtra =  (total *.01) + total;
+                   mDisplayTotal.setText(valueOf(newWithExtra));
+                   personableChecked = true;
+                }
+            }
+        });
+
+        fastService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(billOne.getTipAmt() == 0){
+                    return;
+                }
+                if(personable.isChecked()){
+                    double total = (Double.parseDouble(mDisplayTotal.getText().toString()));
+                   double newWithExtra =  (total *.01) + total;
+                   mDisplayTotal.setText(valueOf(newWithExtra));
+                   fastServiceChecked = true;
+                }
+            }
+        });
+
+
+
+    }
 
 }
 
